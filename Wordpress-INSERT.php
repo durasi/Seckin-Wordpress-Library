056c8e70bd0cc450869e01b2304844a53845eb12
Seçkin Durası <bilgi@seckin.co>
2021-03-13 14:27:45 +0300

    Wordpress-INSERT

---
+++ Wordpress-INSERT.php
@@ -0,0 +1,22 @@
+<!--
+//2021-03-13
+//www.seckin.co
+//Seçkin Durası 
+//https://github.com/durasi/Seckin-Wordpress-Library
+-->
+
+<?
+	$datenow=time();
+    $ip=$_SERVER["REMOTE_ADDR"];
+	$url = end(explode('/', trim($_SERVER['REQUEST_URI'], '/')));
+	try{
+		global $wpdb;  
+		$sql=$wpdb->prepare("INSERT INTO seckin SET datenow = '{$datenow}', ip='".$_SERVER["REMOTE_ADDR"]."', url = '{$url}'"); 
+		$sqldata=$wpdb->query($sql); 
+		if($sqldata): 
+			echo "Success.";
+			else:
+			throw new Exception("Erorr!!!");
+			endif;
+		}catch(Exception $a){ echo  "Erorr :".$a->getMessage(); }
+?>
